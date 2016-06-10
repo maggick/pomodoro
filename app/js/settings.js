3b1d1fc6f5e2ef75474599a6c0721fe20f96cd61
Matthieu Keller <keller.mdpa@gmail.com>
2016-06-10 11:04:28 +0200

    Settings window back to work

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var ipc = require('ipc');
+const {ipcRenderer} = require('electron');
 
 var closeEl = document.querySelector('.close');
 closeEl.addEventListener('click', function (e) {
-  ipc.send('close-settings-window');
+  ipcRenderer.send('close-settings-window');
 });
 
 var configuration = require('../configuration');
